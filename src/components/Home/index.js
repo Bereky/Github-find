770eb29daf7ae116e52c68198ccb474c1bc9211a
bereky <bereketlemma4@gmail.com>
2022-09-15 23:37:57 +0300

    update landing text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ const Home = () => {
     return ( 
         <div className="container">
             <h1 className='heading'>Who is "x" on github?</h1>
-            <h3 className='description'>Get information about developers</h3>
+            <h3 className='description'>Let me tell you about developers</h3>
             <span className="animation-container"></span>
         </div>
      );
